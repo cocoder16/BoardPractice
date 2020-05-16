b8829bc133bf2debfa2001f3ebb05a9f882e7826
cocoder <cocoder16@gmail.com>
2020-05-16 13:48:28 +0900

    add jest

--- src/client/pages/Home.js
+++ src/client/pages/Home.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class Home extends Component {
   render() {
     return (
-        <h1>Hello React</h1>
+        <h1 data-testid='h1tag' className='fancy-h1'>Hello React</h1>
     );
   }
 };
